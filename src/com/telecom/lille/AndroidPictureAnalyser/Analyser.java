27bd0c904e855182a5674c43921db3dc4bb0a01f
caronpe <dark.karak@gmail.com>
2015-10-14 09:35:56 +0200

    Ajout de la class Analyser

---
+++ src/com/telecom/lille/AndroidPictureAnalyser/Analyser.java
@@ -0,0 +1,16 @@
+package com.telecom.lille.AndroidPictureAnalyser;
+
+import android.net.Uri;
+
+public class Analyser {
+	
+	ListAdaptater listAdaptater;
+	Uri image;
+	
+	public Analyser(ListAdaptater listAdaptater, Uri image){
+		this.listAdaptater = listAdaptater;
+		this.image = image;
+	}
+	
+	
+}
