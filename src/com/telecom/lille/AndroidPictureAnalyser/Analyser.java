a23bfd7e65778bc1f03a5f2d8a0a31c32fbc7d76
caronpe <dark.karak@gmail.com>
2015-10-14 10:55:08 +0200

    Création de Mat par image

--- src/com/telecom/lille/AndroidPictureAnalyser/Analyser.java
+++ src/com/telecom/lille/AndroidPictureAnalyser/Analyser.java
@@ -3,6 +3,11 @@ package com.telecom.lille.AndroidPictureAnalyser;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import com.example.tpandroid1.R;
+
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -10,13 +15,20 @@ import android.provider.MediaStore;
 public class Analyser {
 	
 	ListAdaptater listAdaptater;
-	Bitmap mainImage, comparable;
+	Bitmap mainImage, compareImage;
+	Mat mainImageMat, compareImageMat;
 	ResultActivity resultActivity;
 	
 	public Analyser(ListAdaptater listAdaptater, ResultActivity resultActivity, Uri imageUri) throws FileNotFoundException, IOException{
 		this.listAdaptater = listAdaptater;
 		this.resultActivity = resultActivity;
+		//chargement des images à partir de l'URI de l'image
 		mainImage = MediaStore.Images.Media.getBitmap(resultActivity.getContentResolver(), imageUri);
+		compareImage = MediaStore.Images.Media.getBitmap(resultActivity.getContentResolver(), imageUri);
+		this.mainImageMat = new Mat(); 
+		this.compareImageMat = new Mat();
+		Utils.bitmapToMat(mainImage, mainImageMat);
+		Utils.bitmapToMat(compareImage, compareImageMat);
 	}
 	
 	
